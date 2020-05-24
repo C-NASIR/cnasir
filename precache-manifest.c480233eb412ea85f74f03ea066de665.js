018867301127735633b0b54eea9154ec72e050e6
C-Nasir <Nasrulahi90@gmail.com>
2020-05-24 16:27:40 -0500

    Updates

---
+++ precache-manifest.c480233eb412ea85f74f03ea066de665.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d984ec8fe0c92d69d6a2b5f97709581",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95aa80ecc9201d995039",
+    "url": "/static/js/2.5e1da98d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5e1da98d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e19cdfd643ab9a72802",
+    "url": "/static/js/main.63c98687.chunk.js"
+  },
+  {
+    "revision": "6b4394638b6f091dcef2",
+    "url": "/static/js/runtime-main.a49db578.js"
+  },
+  {
+    "revision": "ca35c262fa151e592c98b255e091ce55",
+    "url": "/static/media/Nasir.ca35c262.jpg"
+  },
+  {
+    "revision": "3c00f9c51a273946bf3b6de35d04a231",
+    "url": "/static/media/wearingMask.3c00f9c5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cnasir/precache-manifest.c15749f935a9d6f0e1d39173a8046000.js"
+  "/precache-manifest.c480233eb412ea85f74f03ea066de665.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cnasir/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
