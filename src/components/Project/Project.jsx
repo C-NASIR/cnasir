cac09c7220b94920f52a08feaf82a4de2eec915b
C-NASIR <cnasir22.0@gmail.com>
2022-12-06 21:42:19 -0600

    added link

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-
+import Pill from "../pill/Pill";
 import * as styles from "./style.module.css";
 
 const Project = ({ websiteUrl }) => {
   return (
     <div className={styles.project}>
       <div className={styles.info}>
-        <h3> Music App </h3>
+        <h2> Music App </h2>
         <p> This is a music application built with React js </p>
         <p> It is Interactive. Go a head. </p>
         <h3> Uses </h3>
-        <ul>
+        <ul className={styles.list}>
           <li>
             You can Scroll through favorite artists and click on the one you
             like
@@ -18,13 +18,16 @@ const Project = ({ websiteUrl }) => {
           <li> You can search for an artists</li>
         </ul>
         <h3> Tech Stack </h3>
-        <ul>
-          <li> Create React App (ReactJS) </li>
-          <li> JavaScript, Modular CSS</li>
-          <li> Consuming Rest API </li>
-        </ul>
-        <p> Application Code </p>
-        <button> Visit The Web Application </button>
+        <div className={styles.tech}>
+          <Pill text="Create React App" />
+          <Pill text="JavaScript " />
+          <Pill text="Modular CSS" />
+          <Pill text="Restful API" />
+        </div>
+        <a className={styles.sourcecode} href="" Source code>
+          Source Code
+        </a>
+        <p className={styles.weblink}> Visit The Web Application </p>
       </div>
       <div className={styles.frame}>
         <iframe src={websiteUrl} />
